0ad01063f9eb898b038a6594cef797edb7121261
jucamgara <150453813+jucamgara@users.noreply.github.com>
2023-11-10 10:06:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,10 @@ st.write('El texto escrito es', texto)
 st.subheader("Ahora usemos 2 Columnas")
 
 col1, col2 = st.columns(2)
+with col1:
+    st.subheader("Esta es la primera columna")
+    st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+    resp = st.checkbox('Estoy de acuerdo')
+    if resp:
+       st.write('Correcto!')
 
