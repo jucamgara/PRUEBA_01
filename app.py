b63649603508a1a671a1e5a71dfca709549f2182
jucamgara <150453813+jucamgara@users.noreply.github.com>
2023-11-10 09:52:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 import streamlit as st
 st.title("Mi primera app")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
