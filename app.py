6a995dc07429969235f8078fb2cbbed1b312d0c3
jucamgara <150453813+jucamgara@users.noreply.github.com>
2023-11-10 09:58:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ st.title("Mi primera app")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.write("Facilmente puedo realizar backend y frontend.")
-image = Image.open('Interfaces Mult2.png')
+image = Image.open('mario.jpg')
 
 st.image(image, caption='Interfaces multimodales')
